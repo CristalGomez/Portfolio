eeb8ae41c3ad72e2780cf866540deff876fb0353
Cristal Gomez <cristalsg1@gmail.com>
2019-04-02 09:21:31 -0500

    parallax

---
+++ html-routes/html-routes.js
@@ -0,0 +1,14 @@
+// Dependencies
+var path = require("path");
+var express = require("express")
+// var db = require("../models");
+
+module.exports = function (app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  app.get("/", function (req, res) {
+    res.render("index");
+  })
+
+};
---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var path = require("path")
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars")
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+// var env = require("dotenv").config()
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname, './public/js')))
+app.use(express.static(path.join(__dirname, './public/images')))
+app.use(express.static(path.join(__dirname, './parallax.js-1.5.0')))
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+app.set('views', __dirname + '/views');
+
+
+// Routes
+require("./html-routes/html-routes")(app);
+
+var syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+    syncOptions.force = true;
+}
+
+app.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+});
